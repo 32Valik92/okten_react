6f24af715508e4e82c02ebe10b64b9c7759cc32d
32Valik92 <valokstrukalo@gmail.com>
2023-04-30 16:05:40 +0300

    Base Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import {useDispatch, useSelector} from "react-redux";
+
 const App = () => {
+    let store = useSelector(state => state)
+    const dispatch = useDispatch();
+
+    const increment = () => {
+        dispatch({type: 'INC', payload: 2})
+    }
+
+    const decrement = () => {
+        dispatch({type: 'DEC', payload: 2})
+    }
+
+    const res = () => {
+        dispatch({type: 'RESET', payload: 2})
+    }
+
     return (
         <div>
 
+            <h1>{store}</h1>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <button onClick={res}>reset</button>
         </div>
     );
 };
